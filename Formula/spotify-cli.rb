d626660cb1e23314996bf462959b0ecbe2da5c68
Anirudh Konidala <kidskodingclub@gmail.com>
2024-12-11 16:53:04 -0600

    Updated rb script for homebrew install

--- Formula/spotify-cli.rb
+++ Formula/spotify-cli.rb
@@ -1,8 +1,7 @@
 class SpotifyCli < Formula
   desc "This is the Homebrew tap for spotify-cli, a command-line tool for Spotify."
   homepage "https://github.com/kidskoding/spotify-cli"
-  url "https://github.com/kidskoding/spotify-cli/releases/download/v0.1.3-alpha/spotify-cli.tar.gz"
-  sha256 "0447300f836b44b7a8af3a403906d369bb0f4b59d414023cd4e5ffbae2776057"
+  head "https://github.com/kidskoding/spotify-cli.git"
 
   def install
     bin.install "spotify-cli" => "spotify"
